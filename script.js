789ba5ca415616c96d395198287f80d88ea3fc55
sdvhujf <sithuru15@gmail.com>
2024-12-05 17:39:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
